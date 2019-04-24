b61c87d0d33d1f40e8c205351f9393ea182a868c
Yurizal Susanto <yursan9@pm.me>
2019-04-24 20:56:31 +0700

    Add new routing rules for Blog

--- application/config/routes.php
+++ application/config/routes.php
@@ -49,5 +49,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | Examples:	my-controller/index	-> my_controller/index
 |		my-controller/my-method	-> my_controller/my_method
 */
+$route['blog/(:any)'] = 'blog/view/$1';
+$route['blog'] = 'blog';
 $route['default_controller'] = 'pages/view';
 $route['(:any)'] = 'pages/view/$1';
